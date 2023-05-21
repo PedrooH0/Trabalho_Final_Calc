5b376def00ea188f05c1b8e766e5704d42117ebb
Pedro Henrique <124939591+PedrooH0@users.noreply.github.com>
2023-05-21 18:34:48 -0300

    Add files via upload

---
+++ Projeto-POO/src/main/java/br/edu/telasdasplanas/driverClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.telasdasplanas;
+
+/**
+ *
+ * @author MANUELA
+ */
+public class driverClass {
+    public static void main(String[] args) {
+        DadosAreaCirculo frame = new DadosAreaCirculo();
+        frame.setVisible(true);
+    }
+}
